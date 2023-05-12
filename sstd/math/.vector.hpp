ff0925f5dcb5ae1687786ba8127b911105ef4158
Gahjouyooj <tyiwei0@gmail.com>
2023-05-12 22:05:35 +0800

    Chore: Refactored .vector.hpp Feat: Added optimisations for 2-element vector and 3-element vectors

--- sstd/math/.vector.hpp
+++ sstd/math/.vector.hpp
@@ -28,313 +28,323 @@
 #define SSTD_VECTOR2_HPP
 
 #include <algorithm>
+#include <initializer_list>
 #include <iterator>
-#include <sstd/.io/printCont.hpp>
 #include <stdexcept>
 
 namespace sstd {
 
-/// @brief 2-dimensional vector class
+template <class T, size_t N>
+class vector {
+ protected:
+  T vec[N] = {0};
+
+ public:
+  vector() = default;
+  template <class Iter>
+  vector(const Iter begin, const Iter end) {
+    if (end - begin != N) {
+      throw std::invalid_argument("Size of range is wrong");
+    }
+    std::copy(begin, end, std::begin(vec));
+  }
+  vector(const std::initializer_list<T> init) {
+    if (init.size() != N) {
+      throw std::invalid_argument("Size of range is wrong");
+    }
+    std::copy(init.begin(), init.end(), std::begin(vec));
+  }
+
+  inline T at(const size_t index) const { return vec[index]; }
+  inline void at(const size_t index, const T value) { vec[index] == value; }
+
+  inline T* data() { return vec; }
+  inline auto begin() { return std::begin(vec); }
+  inline auto end() { return std::end(vec); }
+  inline auto cbegin() const { return std::cbegin(vec); }
+  inline auto cend() const { return std::cend(vec); }
+  inline auto rbegin() { return std::rbegin(vec); }
+  inline auto rend() { return std::rend(vec); }
+  inline auto crbegin() const { return std::crbegin(vec); }
+  inline auto crend() const { return std::crend(vec); }
+  inline size_t size() const { return N; }
+  inline size_t sizeOf() const { return sizeof(*this); }
+
+  inline T operator[](size_t index) const { return vec[index]; }
+  inline T& operator[](size_t index) { return vec[index]; }
+  vector<T, N> operator+(const vector<T, N> _vector) const {
+    vector<T, N> copy = *this;
+    for (size_t i = 0; i < N; ++i) {
+      copy[i] += _vector[i];
+    }
+    return copy;
+  }
+  vector<T, N> operator-(const vector<T, N> _vector) const {
+    vector<T, N> copy = *this;
+    for (size_t i = 0; i < N; ++i) {
+      copy[i] -= _vector[i];
+    }
+    return copy;
+  }
+  template <class A>
+  vector<T, N> operator*(const A _num) const {
+    vector<T, N> copy = *this;
+    for (size_t i = 0; i < N; ++i) {
+      copy[i] *= _num;
+    }
+    return copy;
+  }
+  vector<T, N> operator+=(const vector<T, N> _vector) {
+    for (size_t i = 0; i < N; ++i) {
+      vec[i] += _vector[i];
+    }
+    return *this;
+  }
+  vector<T, N> operator-=(const vector<T, N> _vector) {
+    for (size_t i = 0; i < N; ++i) {
+      vec[i] -= _vector[i];
+    }
+    return *this;
+  }
+  template <class A>
+  vector<T, N> operator*=(const A _num) {
+    for (size_t i = 0; i < N; ++i) {
+      vec[i] *= _num;
+    }
+    return *this;
+  }
+  vector<T, N> operator++() {
+    for (size_t i = 0; i < N; ++i) {
+      ++vec[i];
+    }
+    return *this;
+  }
+  vector<T, N> operator--() {
+    for (size_t i = 0; i < N; ++i) {
+      --vec[i];
+    }
+    return *this;
+  }
+  vector<T, N> operator++(int) {
+    vector<T, N> copy = *this;
+    for (size_t i = 0; i < N; ++i) {
+      ++copy[i];
+    }
+    return copy;
+  }
+  vector<T, N> operator--(int) {
+    vector<T, N> copy = *this;
+    for (size_t i = 0; i < N; ++i) {
+      --copy[i];
+    }
+    return copy;
+  }
+
+#if defined(SSTD_PRINTCONT_HPP)
+  void print() const { print(cbegin(), cend()); }
+#endif
+};
+
 template <class T>
-class vector2 {
+class vector<T, 2> {
  protected:
   T vec[2] = {0};
 
  public:
-  /// @brief Default constructor. Zero initialises vector
-  vector2() = default;
-
-  /// @brief Constructs vector
-  /// @param begin Range begin iterator
-  /// @param end Range end iterator
+  vector() = default;
   template <class Iter>
-  vector2(const Iter begin, const Iter end) {
+  vector(const Iter begin, const Iter end) {
     if (end - begin != 2) {
-      throw std::invalid_argument("Size of container is wrong");
+      throw std::invalid_argument("Size of range is wrong");
     }
     std::copy(begin, end, std::begin(vec));
-  };
-
-  /// @brief Constructs vector
-  /// @param first First element of vector
-  /// @param second Second element of vector
-  vector2(const T first, const T second) noexcept {
+  }
+  vector(const T first, const T second) {
     vec[0] = first;
     vec[1] = second;
   }
 
-  /// @brief Gets value from vector
-  /// @param index Index of value
-  /// @return value
-  inline T at(const size_t index) const {
-    if (abs(index) > 1) {
-      throw std::invalid_argument("Index not in vector");
-    }
-    return vec[index];
-  }
+  inline T at(const size_t index) const { return vec[index]; }
+  inline void at(const size_t index, const T value) { vec[index] = value; }
 
-  /// @brief Replaces value in vector
-  /// @param index Index of value
-  /// @param value Value to be replace with
-  inline void at(const size_t index, T value) {
-    if (abs(index) > 1) {
-      throw std::invalid_argument("Index not in vector");
-    }
-    vec[index] = value;
-  }
-
-  /// @brief Gives access to the underlying array of this
-  /// @return Underlying array
   inline T* data() { return vec; }
+  inline auto begin() { return std::begin(vec); }
+  inline auto end() { return std::end(vec); }
+  inline auto cbegin() const { return std::cbegin(vec); }
+  inline auto cend() const { return std::cend(vec); }
+  inline auto rbegin() { return std::rbegin(vec); }
+  inline auto rend() { return std::rend(vec); }
+  inline auto crbegin() const { return std::crbegin(vec); }
+  inline auto crend() const { return std::crend(vec); }
+  inline T first() const { return vec[0]; }
+  inline T second() const { return vec[1]; }
+  inline size_t size() const { return 2; }
+  inline size_t sizeOf() const { return sizeof(*this); }
 
-  /// @brief Gets first value of the vector2
-  /// @return First value of the vector2
-  inline T cfirst() const { return vec[0]; }
-
-  /// @brief Gets first reference of the vector2
-  /// @return First reference of the vector2
-  inline T& first() { return vec[0]; }
-
-  /// @brief Gets first value of the vector2
-  /// @return First value of the vector2
-  inline T csecond() const { return vec[1]; }
-
-  /// @brief Gets second reference of the vector2
-  /// @return Second reference of the vector2
-  inline T& second() { return vec[1]; }
-
-  /// @brief Gets size of vector2
-  /// @return 2
-  inline size_t size() { return 2; }
-
-  /// @brief Gets size of vector2 in bytes
-  /// @return Size of vector2 in bytes
-  inline size_t sizeof() { return sizeof(*this); }
-
-  /// @brief Gets element at specified index
-  /// @param index Index of element requested
-  /// @return Value of element at specified index
   inline T operator[](size_t index) const { return vec[index]; }
-
-  /// @brief References element at specified index
-  /// @param index Index of element requested
-  /// @return Reference to element at specified index
   inline T& operator[](size_t index) { return vec[index]; }
-
-  /// @brief Adds a vector to this
-  /// @param _vector Vector to be added to this
-  /// @return This after addition
-  /// @warning <T> will not be typecasted to <R> even if there's a loss of
-  /// precision
-  template <class R>
-  vector2<T> add(vector2<R> _vector) {
+  vector<T, 2> operator+(const vector<T, 2> _vector) const {
+    return vector<T, 2>{vec[0] + _vector[0], vec[1] + _vector[1]};
+  }
+  vector<T, 2> operator-(const vector<T, 2> _vector) const {
+    return vector<T, 2>{vec[0] - _vector[0], vec[1] - _vector[1]};
+  }
+  template <class A>
+  vector<T, 2> operator*(const A _num) const {
+    return vector<T, 2>{vec[0] * _num, vec[1] * _num};
+  }
+  vector<T, 2> operator+=(const vector<T, 2> _vector) {
     vec[0] += _vector[0];
     vec[1] += _vector[1];
     return *this;
   }
-
-  /// @brief Subtracts a vector from this
-  /// @param _vector Vector to be subtracted from this
-  /// @return This after subtraction
-  /// @warning <T> will not be typecasted to <R> even if there's a loss of
-  /// precision
-  template <class R>
-  vector2<T> sub(vector2<R> _vector) {
+  vector<T, 2> operator-=(const vector<T, 2> _vector) {
     vec[0] -= _vector[0];
-    vec[2] -= _vector[1];
+    vec[1] -= _vector[1];
     return *this;
   }
-
-  /// @brief Multiplies this by a scalar
-  /// @param toMult Scalar to be multiplied to this
-  /// @return This after multiplication
-  /// @warning <T> will not be typecasted to <R> even if there's a loss of
-  /// precision
-  template <class R>
-  vector2<T> mult(R toMult) {
-    vec[0] *= toMult;
-    vec[1] *= toMult;
+  template <class A>
+  vector<T, 2> operator*=(const A _num) {
+    vec[0] *= _num;
+    vec[1] *= _num;
     return *this;
+  }
+  vector<T, 2> operator++() {
+    ++vec[0];
+    ++vec[1];
+    return *this;
+  }
+  vector<T, 2> operator--() {
+    --vec[0];
+    --vec[1];
+    return *this;
+  }
+  vector<T, 2> operator++(int) {
+    vector<T, 2> copy = *this;
+    ++vec[0]++ vec[1];
+    return copy;
+  }
+  vector<T, 2> operator--(int) {
+    vector<T, 2> copy = *this;
+    --vec[0];
+    --vec[1];
+    return copy;
   }
 
 #if defined(SSTD_PRINTCONT_HPP)
-  /// @brief Prints this vector
-  void print() const { std::cout << vec[0] << "\n" << vec[1]; }
+  void print() const { sstd::print(cbegin(), cend()); }
 #endif
 };
 
-/// @brief 3-dimensional vector class
 template <class T>
-class vector3 {
+class vector<T, 3> {
  protected:
   T vec[3] = {0};
 
  public:
-  /// @brief Default constructor. Zero initialises vector
-  vector3() = default;
-
-  /// @brief Constructrs vector
-  /// @param begin Range begin iterator
-  /// @param end Range end iterator
+  vector() = default;
   template <class Iter>
-  vector3(const Iter begin, const Iter end) {
+  vector(const Iter begin, const Iter end) {
     if (end - begin != 3) {
-      throw std::invalid_argument("Size of container is wrong");
+      throw std::invalid_argument("Size of range is wrong");
     }
     std::copy(begin, end, std::begin(vec));
   }
-
-  /// @brief Constructs vector
-  /// @param first First element of vector
-  /// @param second Second element of vector
-  /// @param third Third element of vector
-  vector3(const T first, const T second, const T third) noexcept {
+  vector(const T first, const T second, const T third) {
     vec[0] = first;
     vec[1] = second;
     vec[2] = third;
   }
 
-  /// @brief Gets value from vector
-  /// @param index Index of value
-  /// @return value
-  inline T at(const size_t index) const {
-    if (abs(index) > 1) {
-      throw std::invalid_argument("Index not in vector");
-    }
-    return vec[index];
-  }
+  inline T at(const size_t index) const { return vec[index]; }
+  inline void at(const size_t index, const T value) { vec[index] = value; }
 
-  /// @brief Replaces value in vector
-  /// @param index Index of value
-  /// @param value Value to be replaced with
-  inline void at(const size_t index, T value) {
-    if (abs(index) > 1) {
-      throw std::invalid_argument("Index not in vector");
-    }
-    vec[index] = value;
-    return;
-  }
-
-  /// @brief Gives access to the underlying array of this
-  /// @return Underlying array
   inline T* data() { return vec; }
+  inline auto begin() { return std::begin(vec); }
+  inline auto end() { return std::end(vec); }
+  inline auto cbegin() const { return std::cbegin(vec); }
+  inline auto cend() const { return std::cend(vec); }
+  inline auto rbegin() { return std::rbegin(vec); }
+  inline auto rend() { return std::rend(vec); }
+  inline auto crbegin() const { return std::crbegin(vec); }
+  inline auto crend() const { return std::crend(vec); }
+  inline T first() const { return vec[0]; }
+  inline T second() const { return vec[1]; }
+  inline T third() const { return vec[2]; }
+  inline size_t size() const { return 3; }
+  inline size_t sizeOf() const { return sizeof(*this); }
 
-  /// @brief Gets first value of the vector3
-  /// @return First value of the vector3
-  inline T cfirst() const { return vec[0]; }
-
-  /// @brief Gets first reference of the vector3
-  /// @return First reference of the vector3
-  inline T& first() { return vec[0]; }
-
-  /// @brief Gets second value of the vector3
-  /// @return Second value of the vector3
-  inline T csecond() const { return vec[1]; }
-
-  /// @brief Gets second reference of the vector3
-  /// @return Second reference of the vector3
-  inline T& second() { return vec[1]; }
-
-  /// @brief Gets third value of the vector3
-  /// @return Third value of the vector3
-  inline T cthird() const { return vec[2]; }
-
-  /// @brief Gets third reference of the vector3
-  /// @return Third reference of the vector3
-  inline T& third() { return vec[2]; }
-
-  /// @brief Gets size of vector3
-  /// @return 3
-  inline size_t size() { return 3; }
-
-  /// @brief Gets size of vector3 in bytes
-  /// @return Size of vector3 in bytes
-  inline size_t sizeof() { return sizeof(*this); }
-
-  /// @brief Gets element at specified index
-  /// @param index Index of element requested
-  /// @return Value of element at specified index
   inline T operator[](size_t index) const { return vec[index]; }
-
-  /// @brief References element at specified index
-  /// @param index Index of element requested
-  /// @return Reference to element at specified index
   inline T& operator[](size_t index) { return vec[index]; }
-
-  /// @brief Adds a vector to this
-  /// @param _vector Vector to be added to this
-  /// @return This after addition
-  /// @warning <T> will not be typecasted to <R> even if there's a loss of
-  /// precision
-  template <class R>
-  vector3<T> add(vector3<R> _vector) {
+  vector<T, 3> operator+(const vector<T, 3> _vector) const {
+    return std::vector<T, 3>{vec[0] + _vector[0], vec[1] + _vector[1],
+                             vec[2] + _vector[2]};
+  }
+  vector<T, 3> operator-(const vector<T, 3> _vector) const {
+    return std::vector<T, 3>{vec[0] - _vector[0], vec[1] - _vector[1],
+                             vec[2] - _vector[2]};
+  }
+  template <class A>
+  vector<T, 3> operator*(const A _num) const {
+    return std::vector<T, 3>{vec[0] * _num, vec[1] * _num, vec[2] * _num};
+  }
+  vector<T, 3> operator+=(const vector<T, 3> _vector) {
     vec[0] += _vector[0];
     vec[1] += _vector[1];
     vec[2] += _vector[2];
     return *this;
   }
-
-  /// @brief Subtracts a vector from this
-  /// @param _vector Vector to be subtracted from this
-  /// @return This after subtraction
-  /// @warning <T> will not be typecasted to <R> even if there's a loss of
-  /// precision
-  template <class R>
-  vector3<T> sub(vector3<R> _vector) {
+  vector<T, 3> operator-=(const vector<T, 3> _vector) {
     vec[0] -= _vector[0];
     vec[1] -= _vector[1];
     vec[2] -= _vector[2];
     return *this;
   }
-
-  /// @brief Multiplies this by a scalar
-  /// @param toMult Scalar to be multiplied to this
-  /// @return This after multiplication
-  /// @warning <T> will not be typecasted to <R> even if there's a loss of
-  /// precision
-  template <class R>
-  vector3<T> mult(R toMult) {
-    vec[0] *= toMult;
-    vec[1] *= toMult;
-    vec[2] *= toMult;
+  template <class A>
+  vector<T, 3> operator*=(const A _num) {
+    vec[0] *= _vector[0];
+    vec[1] *= _vector[1];
+    vec[2] *= _vector[2];
     return *this;
+  }
+  vector<T, 3> operator++() {
+    ++vec[0];
+    ++vec[1];
+    ++vec[2];
+    return *this;
+  }
+  vector<T, 3> operator--() {
+    --vec[0];
+    --vec[1];
+    --vec[2];
+    return *this;
+  }
+  vector<T, 3> operator++(int) {
+    vector<T, 3> copy = *this;
+    ++vec[0];
+    ++vec[1];
+    ++vec[2];
+    return copy;
+  }
+  vector<T, 3> operator--(int) {
+    vector<T, 3> copy = *this;
+    --vec[0];
+    --vec[1];
+    --vec[2];
+    return copy;
   }
 
 #if defined(SSTD_PRINTCONT_HPP)
-  /// @brief Prints this vector
-  void print() const {
-    std::cout << vec[0] << "\n" << vec[1] << "\n" << vec[2];
-  }
+  void print() const;
 #endif
 };
 
-template <typename T, typename R>
-vector2<T> add(const vector2<T> base_vec, const vector2<R> _vector) {
-  return vector2<T>(base_vec[0] + _vector[0], base_vec[1] + _vector[1]);
-}
-
-template <typename T, typename R>
-vector2<T> sub(const vector2<T> base_vec, const vector2<R> _vector) {
-  return vector2<T>(base_vec[0] - _vector[0], base_vec[1] - _vector[1]);
-}
-
-template <typename T, typename R>
-vector2<T> mult(const vector2<T> base_vec, const R toMult) {
-  return vector2<T>(base_vec[0] * toMult, base_vec[1] * toMult);
-}
-
-template <typename T, typename R>
-vector2<T> mult(const R toMult, const vector2<T> _vector) {
-  return vector2<T>(_vector[0] * toMult, _vector[1] * toMult);
-}
-
 #if defined(SSTD_PRINTCONT_HPP)
-
-template <typename T>
-void print(vector2<T> base_vec) {
-  std::cout << "(" << base_vec[0] << ", " << base_vec[1] << ")";
+template <typename T, size_t N>
+void print(const vector<T, N> _vector) {
+  print(_vector.cbegin(), _vector.cend());
 }
-
+}
 #endif
 
 }  // namespace sstd
